8ed419a6827d894c262ea6382e557c62d0789d3e
Diego Gonzalez <diego.gf7@icloud.com>
2021-02-06 22:10:17 -0500

    new stuff added

--- scripts/slowreadexercise.js
+++ scripts/slowreadexercise.js
@@ -24,7 +24,7 @@ function nextPassage(){
     "It was a nice beautiful day but then it started to rain.",
     "To pass the time Daniel played sports such as soccer and tennis.",
     "The red glint of paint sparkled under the sun. He had dreamed of owning this car since he was ten, and that dream had become a reality less than a year ago. It was his baby and he spent hours caring for it, pampering it, and fondling over it. She knew this all too well, and that's exactly why she had taken a sludge hammer to it.",
-    "Out of another, I get a lovely view of the bay and a little private wharf belonging to the estate. There is a beautiful shaded lane that runs down there from the house. I always fancy I see people walking in these numerous paths and arbors, but John has cautioned me not to give way to fancy in the least. He says that with my imaginative power and habit of story-making a nervous weakness like mine is sure to lead to all manner of excited fancies and that I ought to use my will and good sense to check the tendency. So I try.",
+    "Out of another, I get a lovely view of the bay and a little private wharf belonging to the estate. There is a beautiful shaded lane that runs down there from the house. I always fancy I see people walking in these numerous paths and arbors, but John has cautioned me not to give way to fancy in the least. He says that with my imaginative power and habit of story-making a nervous weakness like mine is sure to lead to all manner of excited fancies and that I ought to use my will and good sense to check the tendency. So I try."
   ];
   var random = Math.floor(Math.random() * passages.length);
   var passage = passages[random];
