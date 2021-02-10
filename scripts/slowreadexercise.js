1d972362885c1a3b79aeba5fe3b9e36d95842d4c
Diego Gonzalez <diego.gf7@icloud.com>
2021-02-09 19:08:43 -0500

    cool

--- scripts/slowreadexercise.js
+++ scripts/slowreadexercise.js
@@ -19,7 +19,6 @@ function addNext() {
 
 function nextPassage(){
   var passages = [
-    "The fox jumped over the fence, the chickens ran away.",
     "Dave found joy in the daily routine of life. He awoke at the same time, ate the same breakfast and drove the same commute. He worked at a job that never seemed to change and he got home at 6 pm sharp every night. It was who he had been for the last ten years and he had no idea that was all about to change.",
     "It was a nice beautiful day but then it started to rain.",
     "To pass the time Daniel played sports such as soccer and tennis.",
