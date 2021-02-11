09ffc52b41bac6a88d0ede2e077dd598a25c6b81
Diego Gonzalez <diego.gf7@icloud.com>
2021-02-10 19:19:06 -0500

    changed symbols

--- scripts/slowreadexercise.js
+++ scripts/slowreadexercise.js
@@ -25,9 +25,9 @@ function nextPassage(){
     "The peacock is the national bird of India. They have colourful feathers, two legs and a small beak. They are famous for their dance. When a peacock dances it spreads its feathers like a fan. It has a long shiny dark blue neck. Peacocks are mostly found in the fields they are very beautiful birds. The females are known as ‘Peahen1. Their feathers are used for making jackets, purses etc. We can see them in a zoo.",
     "The stars are tiny points of light in the space. On a clear night we can see around 2,000 to 3,000 stars without using a telescope. Stars look tiny in the sky because they are far away from the Earth. In ancient times the sky watchers found patterns of stars in the sky.These astronauts Neil Armstrong and patterns of people and the creatures from the myths and the legends. As the Earth spins from east to west the stars also appear to cross from east to west. The stars are made up of gases.",
     "Mount Rushmore known as The Presidents Mountain is located in the Black Hills of South Dakota. Doane Robison thought of
-    the idea to have a stoned carved monument as one way to attract people froom all over the country to come to South Dakota.",
+    the idea to have a stoned carved monument, one way to attract people froom all over the country to come to the state.",
     "An ice age is a time period when the temperature of the Earth's surface and atmosphere is greatly reduced so that there is a presence
-    or expansion of continental ice sheets, polar ice sheets and alpine glaciers in the southern and northern hemispheres."
+    or expansion to continental ice sheets, polar ice sheets and alpine glaciers inside the southern and northern hemispheres."
   ];
   var random = Math.floor(Math.random() * passages.length);
   var passage = passages[random];
